b8d81274d572f2594dc756c32d57eb10ee25bbff
MarijusIL <78425010+MarijusIL@users.noreply.github.com>
2021-08-14 14:44:13 +0300

    initial

---
+++ bank.php
@@ -0,0 +1,25 @@
+<?php
+require __DIR__ . '/functions.php';
+define('URL', 'http://localhost/lape/bankas/bank.php');
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Labai Patikimas Bankas</title>
+</head>
+
+<body>
+    <form action="http://localhost/lape/bankas/bank.php" method="post">
+
+    </form>
+</body>
+
+</html>
---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php
+
+function initialize()
+{
+    if (!file_exists(__DIR__ . '/accounts.json')) {
+    }
+}
+
+function getAccounts(): array
+{
+
+    return [];
+}
+
+function createAccount()
+{
+}
+
+function deleteAccount()
+{
+}
+
+function getAmount()
+{
+}
+
+function setAmount(array $acc, string $operation): void
+{
+}
